82fdb6cfc3a3f387104b1275b6144a80c424ba7c
Michaela Links <michaela.links@avanade.com>
2022-05-23 15:12:06 +0100

    Working Tutorial Version - DB and Index page

---
+++ TodoItems/Migrations/20220523140233_WebpageTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoItems.Migrations
+{
+    public partial class WebpageTest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
